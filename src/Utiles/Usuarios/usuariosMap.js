da41d30c3aec201227bacd5e6c4cb7ff1343d5fa
martinbejarano <martinbejarano12@outlook.com>
2025-06-09 13:10:51 -0300

    .

--- src/Utiles/Usuarios/usuariosMap.js
+++ src/Utiles/Usuarios/usuariosMap.js
@@ -5,8 +5,8 @@ const {
 } = require("./usuariosPerfiles");
 
 const users = new Map([
-  ["5493876147003", { perfil: perfilCelulandiaDev, nombre: "Martin" }],
-  ["5491162948395", { perfil: perfilCelulandiaDev, nombre: "Federico" }],
+  ["5493876147003", { perfil: perfilCelulandia, nombre: "Martin" }],
+  ["5491162948395", { perfil: perfilCelulandia, nombre: "Federico" }],
   ["5491154709252", { perfil: perfilCelulandia, nombre: "Ezequiel" }],
   ["5491126032204", { perfil: perfilCelulandia, nombre: "Ventas" }],
   ["5491165000590", { perfil: perfilCelulandia, nombre: "Nicolas" }],
