db760914734fd4277681aa3de5c6d32f07582f2a
Federico Maidan <fede.maidan@gmail.com>
2025-06-10 01:17:51 -0300

    puerto 3002

--- src/Utiles/Mensajes/whatsapp.js
+++ src/Utiles/Mensajes/whatsapp.js
@@ -60,7 +60,7 @@ const connectToWhatsApp = async () => {
     return sock;
 };
 
-app.listen(3000, () => console.log('Servidor corriendo en http://localhost:3000/qr'));
+app.listen(3002, () => console.log('Servidor corriendo en http://localhost:3000/qr'));
 
 // Exporta la función para conectar a WhatsApp
 module.exports = connectToWhatsApp;
