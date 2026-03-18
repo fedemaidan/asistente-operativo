27965d193407530f5532795bd0ffa8af6f3ff2a2
martinbejarano <martinbejarano12@outlook.com>
2026-03-18 12:26:25 -0300

    lid eze

--- src/Utiles/Usuarios/usuariosMap.js
+++ src/Utiles/Usuarios/usuariosMap.js
@@ -18,6 +18,7 @@ const users = new Map([
   ["5491162948395", { perfil: perfilCelulandiaDev, nombre: ["Federico"] }],
   ["5491136322541", { perfil: perfilDrive, nombre: ["Facu"] }],
   ["5491154709252", { perfil: perfilCelulandia, nombre: ["Ezequiel"] }],
+  ["140746145439868", { perfil: perfilCelulandia, nombre: ["Ezequiel"]}],
   ["5491126032204", { perfil: perfilCelulandia, nombre: ["Matias", "Naomi"] }],
   ["5491165000590", { perfil: perfilCelulandia, nombre: ["Nicolas"] }],
   ["5493424421565", { perfil: perfilCelulandia, nombre: ["Matias"] }],
